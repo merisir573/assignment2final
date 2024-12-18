1fa7c6af29833d49f3b1c96d85a3a2b9048ed58b
Mert Erişir <aikosstory@gmail.com>
2024-12-18 19:26:47 +0300

    Delete query-listings/src directory

--- query-listings/src/query-listings/query-listings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QueryListingsService } from './query-listings.service';
-import { QueryListingsController } from './query-listings.controller';
-
-@Module({
-  providers: [QueryListingsService],
-  controllers: [QueryListingsController]
-})
-export class QueryListingsModule {}
