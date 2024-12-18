df1836bf42fd2171bac420852b63760340439b1f
Mert Erişir <aikosstory@gmail.com>
2024-12-18 19:26:16 +0300

    Delete insert-listing/src directory

--- insert-listing/src/insert-listing/insert-listing.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InsertListingService } from './insert-listing.service';
-import { InsertListingController } from './insert-listing.controller';
-
-@Module({
-  providers: [InsertListingService],
-  controllers: [InsertListingController]
-})
-export class InsertListingModule {}
